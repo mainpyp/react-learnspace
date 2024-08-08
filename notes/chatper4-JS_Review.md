2b77fe5fd35e0d1bb058f82be67b101b33381d8b
Adrian H. <61416203+mainpyp@users.noreply.github.com>
2024-08-08 10:13:49 +0200

    Adds first 4 Video notes to JS Review Chapter

--- notes/chatper4-JS_Review.md
+++ notes/chatper4-JS_Review.md
@@ -1,5 +1,23 @@
 # Review of Essential JavaScript for React
+* `Quokka` -> Start on Current File via Command Pallette
+* `const`for not changing values, `var` for dynamic / variable values, `let`is outdated
 
 ## Destructuring Objects and Arrays
+* `const {var1, var2} = object;` -> var1 and var2 must be keys in the object!!
+* `const [firstVar, secondVar, ...restVars] = myArray` -> ... (the rest operator can only be placed last)
 
 ## Rest / Spread Operators
+* Rest Operator: 
+    * Add it to the last variable to add the last elements of an Array to a variable as a list (see above)
+* Spread Operator:
+    * This is used to alter object values
+    * `const newGeneres = [...oldGerneArray, "new added gernre"]` -> "spreads out" the old Array and adds the string
+    * `const newBook = {...book, moviePublicationDate: "2001-12-12"}` -> "spreads out" object functions and adds new key value pair
+
+## String Literals
+* Work very similar as f-strings 
+* Syntax: ```const resultString = `Two plus two is ${2 +2}` ```
+
+## Ternaries
+* if,  else one liners
+* `const returnVal = condition ? returnValIfTrue : returnValIfFalse`
---
+++ projects/02-JS-Review/script.js
@@ -0,0 +1,183 @@
+const data = [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      publicationDate: "1954-07-29",
+      author: "J. R. R. Tolkien",
+      genres: [
+        "fantasy",
+        "high-fantasy",
+        "adventure",
+        "fiction",
+        "novels",
+        "literature",
+      ],
+      hasMovieAdaptation: true,
+      pages: 1216,
+      translations: {
+        spanish: "El señor de los anillos",
+        chinese: "魔戒",
+        french: "Le Seigneur des anneaux",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.52,
+          ratingsCount: 630994,
+          reviewsCount: 13417,
+        },
+        librarything: {
+          rating: 4.53,
+          ratingsCount: 47166,
+          reviewsCount: 452,
+        },
+      },
+    },
+    {
+      id: 2,
+      title: "The Cyberiad",
+      publicationDate: "1965-01-01",
+      author: "Stanislaw Lem",
+      genres: [
+        "science fiction",
+        "humor",
+        "speculative fiction",
+        "short stories",
+        "fantasy",
+      ],
+      hasMovieAdaptation: false,
+      pages: 295,
+      translations: {},
+      reviews: {
+        goodreads: {
+          rating: 4.16,
+          ratingsCount: 11663,
+          reviewsCount: 812,
+        },
+        librarything: {
+          rating: 4.13,
+          ratingsCount: 2434,
+          reviewsCount: 0,
+        },
+      },
+    },
+    {
+      id: 3,
+      title: "Dune",
+      publicationDate: "1965-01-01",
+      author: "Frank Herbert",
+      genres: ["science fiction", "novel", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 658,
+      translations: {
+        spanish: "",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.25,
+          ratingsCount: 1142893,
+          reviewsCount: 49701,
+        },
+      },
+    },
+    {
+      id: 4,
+      title: "Harry Potter and the Philosopher's Stone",
+      publicationDate: "1997-06-26",
+      author: "J. K. Rowling",
+      genres: ["fantasy", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 223,
+      translations: {
+        spanish: "Harry Potter y la piedra filosofal",
+        korean: "해리 포터와 마법사의 돌",
+        bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+        portuguese: "Harry Potter e a Pedra Filosofal",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.47,
+          ratingsCount: 8910059,
+          reviewsCount: 140625,
+        },
+        librarything: {
+          rating: 4.29,
+          ratingsCount: 120941,
+          reviewsCount: 1960,
+        },
+      },
+    },
+    {
+      id: 5,
+      title: "A Game of Thrones",
+      publicationDate: "1996-08-01",
+      author: "George R. R. Martin",
+      genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+      hasMovieAdaptation: true,
+      pages: 835,
+      translations: {
+        korean: "왕좌의 게임",
+        polish: "Gra o tron",
+        portuguese: "A Guerra dos Tronos",
+        spanish: "Juego de tronos",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.44,
+          ratingsCount: 2295233,
+          reviewsCount: 59058,
+        },
+        librarything: {
+          rating: 4.36,
+          ratingsCount: 38358,
+          reviewsCount: 1095,
+        },
+      },
+    },
+  ];
+
+function getBooks() {
+    return data;
+}
+
+function getBookid(id) {
+    return data.find((book) => book.id === id);
+}
+
+const book = getBookid(2); 
+book;
+
+// Destructuring with objects
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } = book;
+title;
+
+// Destructuring with nested objects
+const { goodreads, librarything } = book.reviews;
+goodreads;
+
+// Destructuring with arrays
+const [firstGenre, secondGenre, ...thirdGenre] = genres;
+firstGenre; 
+thirdGenre;
+
+// Spread operator
+const myNewBook = {...book, title: "The Hobbit", pages: 310};
+myNewBook; 
+
+// Template literals (python f-strings)
+const bookInfo = `The book ${title} was written by ${author} in ${publicationDate}`;
+bookInfo;
+
+// Template literals with expressions
+const simpleString = `Two plus two equals ${2 + 2}`;
+const splitted = simpleString.split(" ");
+const [_, , , equals, result] = splitted;
+equals
+result;
+
+// Ternary operator
+const bookSize = pages > 500 ? "Large Book" : "Booklet";
+bookSize;
+
+const infoString = `The book ${title} is a ${bookSize}`;
+infoString;
+
