0bbc28a7a3bbc09984834713083e56d9162989e6
Adrian H. <61416203+mainpyp@users.noreply.github.com>
2024-09-25 12:37:36 +0200

    Updates aday-outside project

--- projects/aday-outside/src/components/Navigation.js
+++ projects/aday-outside/src/components/Navigation.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navigation() {
+  const [isNavActive, setIsNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavActive(!isNavActive);
+  };
+
   return (
     <nav className="navigation">
       <div className="logo">
-      <img src="/images/ado_logo.png" alt="A Day Outside Logo" className="logo-image" />
+        <img src="/images/ado_logo.png" alt="A Day Outside Logo" className="logo-image" />
       </div>
-      <ul className="nav-links">
+      <button className="nav-toggle" onClick={toggleNav}>
+        ☰
+      </button>
+      <ul className={`nav-links ${isNavActive ? 'active' : ''}`}>
         <li><a href="#about">About Us</a></li>
         <li><a href="#workshops">Workshops & Events</a></li>
         <li><a href="#gallery">Gallery</a></li>
