a3bb6eb0f31f327aa69881c852e67d762738ae35
Adrian H. <61416203+mainpyp@users.noreply.github.com>
2024-12-04 16:52:51 +0100

    04-steps Test project for useState

---
+++ projects/04-steps/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+function App() {
+
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  // step is the variable and setStep is a f() that updates the state
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    setStep(Math.max(1, step - 1));
+  };
+
+  const handleNext = () => {
+    setStep(Math.min(3, step + 1));
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <div className="message">{messages[step - 1]}</div>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
